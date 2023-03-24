2880e3c6374fa9d21eec959f3435887838c0d757
Deepak-Urs <gvdeepakurs2202@gmail.com>
2023-03-24 15:44:20 -0700

    CircularQ with Lists started

---
+++ Python/Python DSA/Data Structures/queues/QueueWithCapacity.py
@@ -0,0 +1,68 @@
+class Queue:
+    def __init__(self, maxSize):
+        self.maxSize = maxSize
+        self.items = maxSize * [None]
+        self.start = -1
+        self.top = -1
+
+    def __str__(self):
+        values = [str(x) for x in self.items]
+        return ' '.join(values)
+    
+
+    def isFull(self):
+        if self.top + 1 == self.start:
+            return True
+        elif self.start == 0 and self.top + 1 == self.maxSize:
+            return True
+        else:
+            return False
+
+    def isEmpty(self):
+        if self.top == -1:
+            return True
+        else:
+            return False
+
+    def enqueue(self, value):
+        if self.isFull():
+            return "The Q is full"
+        else:
+            if self.top + 1 == self.maxSize:
+                self.top = 0
+            else:
+                self.top += 1
+                if self.start == -1:
+                    self.start = 0
+            self.items[self.top] = value
+            return "The element is enqueued successfully!"
+
+    def dequeue(self):
+        if self.isEmpty():
+            return "The Q is empty!"
+        else:
+            firstElement = self.items[self.start]
+            start = self.start
+            if self.start == self.top:
+                self.start = -1
+                self.top = -1
+            elif self.start + 1 == self.maxSize:
+                self.start = 0
+            else:
+                self.start += 1
+            self.items[start] = None
+            return firstElement
+
+
+customQ = Queue(3)
+print(customQ.isFull())
+print(customQ.isEmpty())
+
+customQ.enqueue(1)
+customQ.enqueue(2)
+customQ.enqueue(3)
+print(customQ)
+print(customQ.isFull())
+
+print(customQ.dequeue())
+print(customQ)
