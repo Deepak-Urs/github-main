9163ce669a50b8ab4c100e7684a42d7c4932e428
Deepak-Urs <gvdeepakurs2202@gmail.com>
2023-03-27 20:47:23 -0700

    initial commit

---
+++ Javascript/VueJS3/task-manager/src/components/Button.vue
@@ -0,0 +1,13 @@
+<template>
+    <button :style="{background: color}" class="">{{text}}</button>
+</template>
+
+<script>
+export default {
+    name: 'Button-comp',
+    props: {
+        text: String,
+        color: String
+    }
+}
+</script>
---
+++ Javascript/VueJS3/task-manager/src/components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+        <Button class="btn" text="Add Task" color="green"></Button>
+    </header>
+</template>
+
+<script>
+import Button from './Button'
+export default {
+    name: 'Header-comp',
+    props: {
+        title: String
+    },
+    components: {
+        Button
+    }
+}
+</script>
+
+<style scoped>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center; 
+        margin-bottom: 20px;
+        border: 1px solid;
+        padding: 5px
+    }
+
+    .btn {
+    color: inline-block;
+    background: black;
+    color: #fff;
+    border: none;
+    padding: 10px 20px;
+    margin: 5px;
+    border-radius: 5px;
+    cursor: pointer;
+    text-decoration: none;
+    font-size: 15px;
+    font-family: inherit;
+  }
+</style>
