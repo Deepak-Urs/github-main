c28d8364c3c2f728c3114f0ef5943276353e5e1e
Deepak-Urs <gvdeepakurs2202@gmail.com>
2023-04-08 23:41:29 -0700

    Graphs - Introduction and basics

---
+++ Python/Python DSA/Data Structures/Trees/graphs/graph-adjacencyList.py
@@ -0,0 +1,26 @@
+class Graph:
+    def __init__(self):
+        self.adjacency_list = {}
+
+    def add_vertex(self, vertex):
+        if vertex not in self.adjacency_list.keys():    
+            self.adjacency_list[vertex] = []
+            return True
+        return False
+    
+    def add_edge(self, vertex1, vertex2):
+        if vertex1 in self.adjacency_list.keys() and vertex2 in self.adjacency_list.keys():
+            self.adjacency_list[vertex1].append(vertex2)
+            self.adjacency_list[vertex2].append(vertex1)
+            return True
+        return False
+    
+    def print_graph(self):
+        for vertex in self.adjacency_list:
+            print(vertex, ":", self.adjacency_list[vertex])
+
+customG = Graph()
+customG.add_vertex("A")
+#customG.add_vertex("B")
+print(customG.add_edge("A", "B"))
+customG.print_graph()
