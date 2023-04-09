cd895068df5fc86528db9ca2567a47e4c35b9e00
Deepak-Urs <gvdeepakurs2202@gmail.com>
2023-04-08 23:59:17 -0700

    remove edges in graph

--- Python/Python DSA/Data Structures/Trees/graphs/graph-adjacencyList.py
+++ Python/Python DSA/Data Structures/Trees/graphs/graph-adjacencyList.py
@@ -15,12 +15,29 @@ class Graph:
             return True
         return False
     
+    def remove_edge(self,vertex1, vertex2):
+        if vertex1 in self.adjacency_list.keys() and vertex2 in self.adjacency_list.keys():
+            try:
+                self.adjacency_list[vertex1].remove(vertex2)
+                self.adjacency_list[vertex2].remove(vertex1)
+            except ValueError:
+                pass
+            return True
+        return False
+    
     def print_graph(self):
         for vertex in self.adjacency_list:
             print(vertex, ":", self.adjacency_list[vertex])
 
 customG = Graph()
 customG.add_vertex("A")
-#customG.add_vertex("B")
-print(customG.add_edge("A", "B"))
+customG.add_vertex("B")
+customG.add_vertex("C")
+customG.add_vertex("D")
+customG.add_edge("A", "B")
+customG.add_edge("A", "C")
+customG.add_edge("B", "C")
+#customG.print_graph()
+
+print(customG.remove_edge("A", "D"))
 customG.print_graph()
