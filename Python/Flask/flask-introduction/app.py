d5fd13f9d5833d4cf18b44c2162a2a3933f303d8
Deepak-Urs <gvdeepakurs2202@gmail.com>
2023-04-22 20:50:50 -0700

    DB-sqllite setup simple

--- Python/Flask/flask-introduction/app.py
+++ Python/Flask/flask-introduction/app.py
@@ -1,12 +1,27 @@
 #import flask
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy 
+from datetime import datetime
 
 #setup application
 app = Flask(__name__)
+#db setup
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+#DB Model instantiation
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
 
 @app.route('/')
 def index():
     return render_template('./index.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
