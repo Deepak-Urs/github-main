efaa090c30cf5f65409f6e23b93d83796a028300
Deepak-Urs <gvdeepakurs2202@gmail.com>
2023-05-18 23:47:19 -0700

    delete operation

--- Python/Flask/flask-introduction/app.py
+++ Python/Flask/flask-introduction/app.py
@@ -34,7 +34,17 @@ def index():
         tasks = Todo.query.order_by(Todo.date_created).all()
         #.first()
         return render_template('./index.html', tasks=tasks)
-    
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return "Error in deleting a task"
 
 
 if __name__ == "__main__":
