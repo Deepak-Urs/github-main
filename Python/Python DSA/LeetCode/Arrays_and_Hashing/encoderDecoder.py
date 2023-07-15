1f55746a99043248618666d9856715e5e94561b1
Deepak-Urs <gvdeepakurs2202@gmail.com>
2023-07-14 23:59:25 -0400

    dsaOne | Arrays_and_Hashing | encodeDecode

---
+++ Python/Python DSA/LeetCode/Arrays_and_Hashing/encoderDecoder.py
@@ -0,0 +1,25 @@
+#https://leetcode.com/problems/encode-and-decode-strings/
+def encode(arr):
+    idxStr = ""
+    for s in arr:
+        idxStr += str(len(arr)) + "#" + s
+        print('idxStr seen', idxStr)
+    return idxStr
+
+def decode(str):
+    print('i/p str:', str)
+    res, i = [], 0
+
+    while i < len(str):
+        j = i
+        while str[j] != "#":
+            j += 1
+        length = int(str[i:j])
+        res.append(str[j + 1: j + 1 + length])
+        print('res seen after pushing', res)
+        i = j + 1 + length
+    return res
+
+# inst = Solution()
+resStr = encode(['hello', 'world', 'how', 'are', 'you'])
+print(decode(resStr))
