bfae8ea849e9ba93018f38f44827ec1cf446eb78
Deepak-Urs <gvdeepakurs2202@gmail.com>
2023-08-18 23:23:23 -0400

    updated the dragEnd reducer

--- Fullstack/MERN/candy-crush-game/src/store/index.ts
+++ Fullstack/MERN/candy-crush-game/src/store/index.ts
@@ -1,31 +1,49 @@
-import { configureStore, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, configureStore, PayloadAction } from "@reduxjs/toolkit";
+import { dragEndReducer } from "./reducers/dragEnd";
+import { moveBelowReducer } from "./reducers/moveBelow";
 
-const initialState : {
-    board: string[];
-    boardSize: number;
+const initialState: {
+  board: string[];
+  boardSize: number;
+  squareBeingReplaced: Element | undefined;
+  squareBeingDragged: Element | undefined;
 } = {
-    board: [],
-    boardSize: 8 // set board size here
-}
+  board: [],
+  boardSize: 8,
+  squareBeingDragged: undefined,
+  squareBeingReplaced: undefined,
+};
 
 const candyCrushSlice = createSlice({
-    name: "candyCrush",
-    initialState,
-    reducers: {
-        updateBoard: (state, action: PayloadAction<string[]>) => {
-            state.board = action.payload;
-        },
-        moveBelow: moverBelowReducer
+  name: "candyCrush",
+  initialState,
+  reducers: {
+    updateBoard: (state, action: PayloadAction<string[]>) => {
+      state.board = action.payload;
     },
+    dragStart: (state, action: PayloadAction<any>) => {
+      state.squareBeingDragged = action.payload;
+    },
+    dragDrop: (state, action: PayloadAction<any>) => {
+      state.squareBeingReplaced = action.payload;
+    },
+    dragEnd: dragEndReducer,
+    moveBelow: moveBelowReducer,
+  },
 });
 
 export const store = configureStore({
-    reducer: {
-        candyCrush: candyCrushSlice.reducer
-    }
-})
+  reducer: {
+    candyCrush: candyCrushSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
 
-export const {updateBoard, moveBelow } = candyCrushSlice.actions
+export const { updateBoard, moveBelow, dragDrop, dragEnd, dragStart } =
+  candyCrushSlice.actions;
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
