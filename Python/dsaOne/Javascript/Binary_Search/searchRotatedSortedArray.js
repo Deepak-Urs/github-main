1a07517c0b84ba50c95871c9d15d1c6b43de437f
Deepak-Urs <gvdeepakurs2202@gmail.com>
2023-10-15 19:50:01 -0700

    searchRotatedSortedArray | BinarySearch | JS | dsaOne

--- Python/dsaOne/Javascript/Binary_Search/searchRotatedSortedArray.js
+++ Python/dsaOne/Javascript/Binary_Search/searchRotatedSortedArray.js
@@ -1,22 +1,34 @@
-const searchRotatedSortedArray = (nums) => {
-    let l = 0, r = nums.length-1, res = 0;
+//https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+
+const searchRotatedSortedArray = (nums, target) => {
+    let l = 0, r = nums.length - 1
 
     while(l <= r){
-        if(nums[l] < nums[r]) {
-            res = Math.min(nums[l], res)
-            break
+        m = Math.ceil((l+r)/2)
+        if(nums[m] == target) {
+            return m
         }
 
-        m = Math.ceil((r+l)/2)
-        res = Math.ceil(nums[m], res)
         if(nums[l] <= nums[m]) {
-            l = m + 1
-        } else {
-            r = m - 1
+            if(target < nums[l] || target > nums[m]) {
+                l = m + 1
+            }
+            else {
+                r = m - 1
+            }
+        }
+        else {
+            if(target < nums[m] || target > nums[r]) {
+                r = m - 1
+            }
+            else {
+                l = m + 1
+            }
         }
     }
 
-    return res
+    return -1
+
 }
 
-console.log(searchRotatedSortedArray([3,4,5,1,2]))
+console.log(searchRotatedSortedArray([4,5,6,7,0,1,2], 0))
