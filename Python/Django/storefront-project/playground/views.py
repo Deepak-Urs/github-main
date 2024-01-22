fc0f17c5e014453644c1532fc0285c9da3ec68e6
Deepak-Urs <gvdeepakurs2202@gmail.com>
2024-01-21 19:09:23 -0800

    debugging: method-1

--- Python/Django/storefront-project/playground/views.py
+++ Python/Django/storefront-project/playground/views.py
@@ -3,5 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+def calc():
+    return 1
+
 def say_hello(request):
+    x = calc()
+    y = 2
     return render(request, 'hello.html', {'name': 'Deepak'})
