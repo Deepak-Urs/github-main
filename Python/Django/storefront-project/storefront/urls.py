d214056b45988622f6cd61afb7eed22178132abe
Deepak-Urs <gvdeepakurs2202@gmail.com>
2024-01-21 19:17:03 -0800

    debugging: Method2 - django-debug-toolbar

--- Python/Django/storefront-project/storefront/urls.py
+++ Python/Django/storefront-project/storefront/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+import debug_toolbar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('playground/', include('playground.urls')) #playground/hello
+    path('playground/', include('playground.urls')), #playground/hello
+    path("__debug__/", include("debug_toolbar.urls"))
 ]
