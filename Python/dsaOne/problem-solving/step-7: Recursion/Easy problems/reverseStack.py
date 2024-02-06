92aa6908244d891ac872392e33a091f79375f418
Deepak-Urs <gvdeepakurs2202@gmail.com>
2024-02-06 13:59:21 -0800

    Recursion Easy problem: reverseStack

---
+++ Python/dsaOne/problem-solving/step-7: Recursion/Easy problems/reverseStack.py
@@ -0,0 +1,21 @@
+def reverse(stk):
+    if len(stk) == 1:
+        return
+
+    t = stk.pop()
+    reverse(stk)
+
+    insert(stk,t)
+    
+    return stk
+
+def insert(stk, t):
+    if len(stk) == 0:
+        stk.append(t)
+        return
+    
+    t2 = stk.pop()
+    insert(stk, t)
+    stk.append(t2)
+
+print(reverse([1,2,3,4,5]))
