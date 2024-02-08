a614da181d18fabface7817efe6249190343e5b1
Deepak-Urs <gvdeepakurs2202@gmail.com>
2024-02-08 15:33:24 -0800

    Recursion Medium: subsets

---
+++ Python/dsaOne/problem-solving/step-7: Recursion/Medium problems/subsets.py
@@ -0,0 +1,37 @@
+def subsets(ip, op, res):
+    if len(ip) == 0:
+        op = " "
+        return
+    
+    op1 = op
+    op2 = op
+    op2 += ip[0]
+
+    if op1 not in res:
+        res.append(op1)
+
+    if op2 not in res:
+        res.append(op2)
+
+    #print(op1)
+    #print(op2)
+
+    ip = ip[1:]
+
+    subsets(ip, op1, res)
+    subsets(ip, op2, res)
+    
+    return res
+
+def main():
+    ip = input("Enter input string:")
+    op = " "
+    res = []
+    print(subsets(ip, op, res))
+
+main()
+
+# T.C => O(2^n)
+# S.C => O(n * 2^n)
+
+
