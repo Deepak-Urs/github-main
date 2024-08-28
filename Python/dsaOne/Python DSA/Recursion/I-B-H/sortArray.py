29b8681a1c34030b45b9cc6650d8bf7b1737be12
Deepak-Urs <gvdeepakurs2202@gmail.com>
2024-08-27 23:37:22 -0700

    Recursion: sortArray

---
+++ Python/dsaOne/Python DSA/Recursion/I-B-H/sortArray.py
@@ -0,0 +1,22 @@
+def sort(arr):
+    if len(arr) == 1:
+        return arr
+    
+    t = arr.pop()
+    sort(arr)
+
+    insert(arr, t)
+    return arr
+
+def insert(arr, t):
+    if len(arr) == 0 or arr[len(arr)-1] <= t:
+        arr.append(t)
+        return
+    
+    t2 = arr.pop()
+    insert(arr, t)
+    arr.append(t2)
+
+
+print(sort([0,1,5,2]))
+print(sort([0,1,5,2,4,3,11,8,6]))
