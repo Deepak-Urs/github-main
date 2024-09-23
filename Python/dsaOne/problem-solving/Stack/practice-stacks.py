588f2ebdf84a73380fbf84abe7e02d57197b0346
Deepak-Urs <gvdeepakurs2202@gmail.com>
2024-09-23 12:15:11 -0700

    stacks practice - pattern 2

--- Python/dsaOne/problem-solving/Stack/practice-stacks.py
+++ Python/dsaOne/problem-solving/Stack/practice-stacks.py
@@ -1,3 +1,117 @@
+# def NSL(nums):
+#     stk = []
+#     res = []
+
+#     for i in range(len(nums)):
+#         if not len(stk):
+#             res.append(-1)
+#         elif len(stk) and stk[-1][0] < nums[i]:
+#             res.append(stk[-1][1])
+#         elif len(stk) and stk[-1][0] >= nums[i]:
+#             while len(stk) and stk[-1][0] >= nums[i]:
+#                 stk.pop(-1)
+#             if not len(stk):
+#                 res.append(-1)
+#             else:
+#                 res.append(stk[-1][1])
+        
+#         stk.append([nums[i], i])
+    
+#     return res
+
+# def NSR(nums):
+#     stk = []
+#     res = []
+#     l = len(nums)
+
+#     for i in range(len(nums)-1, -1, -1):
+#         if not len(stk):
+#             res.append(l)
+#         elif len(stk) and stk[-1][0] < nums[i]:
+#             res.append(stk[-1][1])
+#         elif len(stk) and stk[-1][0] >= nums[i]:
+#             while len(stk) and stk[-1][0] >= nums[i]:
+#                 stk.pop(-1)
+#             if not len(stk):
+#                 res.append(l)
+#             else:
+#                 res.append(stk[-1][1])
+        
+#         stk.append([nums[i], i])
+    
+#     res.reverse()
+#     return res
+            
+
+# def MAH(nums):
+#     nsl = NSL(nums)
+#     # print(nsl)
+#     nsr = NSR(nums)
+#     # print(nsr)
+
+#     width = []
+#     for i in range(len(nsl)):
+#         width.append(nsr[i]-nsl[i]-1)
+    
+#     area = []
+#     for i in range(len(nums)):
+#         area.append(nums[i]*width[i])
+    
+#     return area
+
+# def MAHBinaryMatrix(nums):
+#     mat = [0 for _ in range(len(nums[0]))]
+#     nR = len(nums)
+#     nC = len(nums[0])
+#     areas = []
+
+
+#     for i in range(nR):
+#         for j in range(nC):
+#             if nums[i][j] == 1:
+#                 mat[j] += 1
+#             else:
+#                 mat[j] = 0
+#         a = MAH(mat)
+#         areas.append(max(a))
+    
+#     return max(areas)
+    
+# print(MAHBinaryMatrix([[0,1,1,0], [1,1,1,1], [1,1,1,1], [1,1,0,0]]))
+
+
+# # print(MAH([6,2,5,4,5,1,6]))
+
+# # # [100, 80, 60, 70, 60, 75,85]
+# # def stockSpan(nums):
+# #     stk = []
+# #     res = []
+
+# #     for i in range(len(nums)):
+# #         if not len(stk):
+# #             res.append(-1)
+# #         elif len(stk) and stk[-1][0] > nums[i]:
+# #             res.append(stk[-1][1])
+# #         elif len(stk) and stk[-1][0] <= nums[i]:
+# #             while len(stk) and stk[-1][0] <= nums[i]:
+# #                 stk.pop(-1)
+# #             if not len(stk):
+# #                 res.append(-1)
+# #             else:
+# #                 res.append(stk[-1][1])
+        
+# #         stk.append([nums[i], i])
+    
+# #     print('res', res)
+# #     #  indices with res
+# #     for k in range(len(res)):
+# #         res[k] = k-res[k]
+    
+# #     return res
+
+# # print(stockSpan([100, 80, 60, 70, 60, 75,85]))
+
+
 # # Nearest Smallest-element to Left / Previous Smaller Element
 # # print(NSL([1,3,2,4])) --> -1, 1, 1, 2
 # # print(NSL([1,0,0,2,7,5,4]))
