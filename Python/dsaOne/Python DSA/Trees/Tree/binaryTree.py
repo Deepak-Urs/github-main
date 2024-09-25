4833d30809f5e05f4c123615abbefa8e864ec7ee
Deepak-Urs <gvdeepakurs2202@gmail.com>
2024-09-25 10:58:03 -0700

    tree traversals: preOrder, inOrder, postOrder

--- Python/dsaOne/Python DSA/Trees/Tree/binaryTree.py
+++ Python/dsaOne/Python DSA/Trees/Tree/binaryTree.py
@@ -5,3 +5,49 @@ class TreeNode:
         self.rightC = None
     
 newTree = TreeNode('Drinks') #O(1), O(1)
+hot = TreeNode('Hot')
+cold = TreeNode('Cold')
+
+tea = TreeNode('Tea')
+coffee = TreeNode('Coffee')
+cola = TreeNode('Cola')
+fanta = TreeNode('Fanta')
+
+
+hot.leftC = tea
+hot.rightC = coffee
+cold.leftC = cola
+cold.rightC = fanta
+
+newTree.leftC = hot
+newTree.rightC = cold
+
+def preOrderTraversal(rootNode): # O(n), O(n)
+    if not rootNode:
+        return
+    print(rootNode.data)
+    preOrderTraversal(rootNode.leftC)
+    preOrderTraversal(rootNode.rightC)
+
+def inOrderTraversal(rootNode): # O(n), O(n)
+    if not rootNode:
+        return
+    inOrderTraversal(rootNode.leftC)
+    print(rootNode.data)
+    inOrderTraversal(rootNode.rightC)
+
+def postOrderTraversal(rootNode):
+    if not rootNode:
+        return
+    postOrderTraversal(rootNode.leftC)
+    postOrderTraversal(rootNode.rightC)
+    print(rootNode.data)
+
+
+
+
+#preOrderTraversal(newTree)
+#inOrderTraversal(newTree)
+postOrderTraversal(newTree)
+
+
