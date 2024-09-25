c9824302c4160cfc2c4179ef467d5853d08b157b
Deepak-Urs <gvdeepakurs2202@gmail.com>
2024-09-25 10:38:59 -0700

    binary Tree creation

---
+++ Python/dsaOne/Python DSA/Trees/Tree/binaryTree.py
@@ -0,0 +1,7 @@
+class TreeNode:
+    def __init__(self, data) -> None:
+        self.data = data
+        self.leftC = None
+        self.rightC = None
+    
+newTree = TreeNode('Drinks') #O(1), O(1)
