b58517e08af9a9b45a27635e708f80e550b39871
Deepak-Urs <gvdeepakurs2202@gmail.com>
2024-09-25 17:45:04 -0700

    CRUD operations Binary Tree

--- Python/dsaOne/Python DSA/Trees/Tree/binaryTree.py
+++ Python/dsaOne/Python DSA/Trees/Tree/binaryTree.py
@@ -62,10 +62,156 @@ def levelOrderTraversal(rootNode): # O(n), O(n)
             customQ.enqueue(root.value.rightC)
 
 
+def searchBT(rootNode, val): # O(n), O(n)
+    if not rootNode:
+        return "BT is empty!"
+    else:
+        customQ = queue.Queue()
+        customQ.enqueue(rootNode)
 
+        while not customQ.isEmpty():
+            root = customQ.dequeue()
+
+            if root.value.data == val:
+                print("Search Success!")
+                return 
+            
+            if root.value.leftC is not None:
+                customQ.enqueue(root.value.leftC)
+
+            if root.value.rightC is not None:
+                customQ.enqueue(root.value.rightC)
+        
+        print("Search Failure")
+        return 
+    
+def insertNodeBT(rootNode, val): # O(n), O(n)
+    newNode = TreeNode(val)
+    if not rootNode:
+        return newNode
+    else:
+        customQ = queue.Queue()
+        customQ.enqueue(rootNode)
+
+        while not customQ.isEmpty():
+            root = customQ.dequeue()
+
+            if root.value.leftC is not None:
+                customQ.enqueue(root.value.leftC)
+            else:
+                root.value.leftC = newNode
+                return "Insertion Success"
+            
+            if root.value.rightC is not None:
+                customQ.enqueue(root.value.rightC)
+            else:
+                root.value.rightC = newNode
+                return "Insertion Success"
+
+def findDeepestNode(rootNode) -> TreeNode:
+    if not rootNode:
+        return "BT empty!"
+    else:
+        customQ = queue.Queue()
+        customQ.enqueue(rootNode)
+
+        while not customQ.isEmpty():
+            root = customQ.dequeue()
+
+            if root.value.leftC is not None:
+                customQ.enqueue(root.value.leftC)
+            
+            if root.value.rightC is not None:
+                customQ.enqueue(root.value.rightC)
+        
+        deepestNode = root.value
+        print('deepestNode data', deepestNode.data)
+        return deepestNode
+
+def deleteDeepestNode(rootNode, dNode) -> None:
+    if not rootNode:
+        return "BT Empty!"
+    else:
+        customQ = queue.Queue()
+        customQ.enqueue(rootNode)
+
+        while not customQ.isEmpty():
+            root = customQ.dequeue()
+            
+            if root.value == dNode:
+                root.value = None
+                return
+            
+            if root.value.leftC:
+                if root.value.leftC is dNode:
+                    root.value.leftC = None
+                    return
+                else:
+                    customQ.enqueue(root.value.leftC)
+            
+            if root.value.rightC:
+                if root.value.rightC:
+                    root.value.rightC = None
+                    return
+                else:
+                    customQ.enqueue(root.value.rightC)
+        
+
+def deleteNodeBT(rootNode, node): # O(n), O(n)
+    print('I/P for deleteNodeBT', node)
+    if not rootNode:
+        return "BT is empty"
+    else:
+        customQ = queue.Queue()
+        customQ.enqueue(rootNode)
+
+        while not customQ.isEmpty():
+            root = customQ.dequeue()
+
+            if root.value.data == node:
+                dNode = findDeepestNode(rootNode)
+                print('dNode seen', dNode.data)
+                # need a check here for leaf and non leaf node deletions
+                root.value.data = dNode.data
+                deleteDeepestNode(rootNode, dNode)
+                return "Node deletion success!"
+            
+            if root.value.leftC is not None:
+                customQ.enqueue(root.value.leftC)
+            
+            if root.value.rightC is not None:
+                customQ.enqueue(root.value.rightC)
+        
+        return "Failed to delete the node"
+
+            
+def deleteEntireBT(rootNode): # O(1)
+    if not rootNode:
+        return "BT Empty!"
+    else:
+        rootNode.data = None
+        rootNode.leftC = None
+        rootNode.rightC = None
+
+
+# DFS
 #preOrderTraversal(newTree)
 #inOrderTraversal(newTree)
 #postOrderTraversal(newTree)
 
+# BFS
+#levelOrderTraversal(newTree)
+    
+# Search
+#searchBT(newTree, "Tea")
+            
+# Insert
+#insertNodeBT(newTree, "Green Tea")
+#levelOrderTraversal(newTree)
+    
+# Delete
+#print(deleteNodeBT(newTree, "Tea"))
+#levelOrderTraversal(newTree)
+        
+deleteEntireBT(newTree)
 levelOrderTraversal(newTree)
-
