35721ebcfd5c6211bb9f94a07d9b8601dd4a64ac
Deepak-Urs <gvdeepakurs2202@gmail.com>
2024-09-27 13:11:44 -0700

    BST node deletion

--- Python/dsaOne/Python DSA/Trees/Binary Search Tree/bst.py
+++ Python/dsaOne/Python DSA/Trees/Binary Search Tree/bst.py
@@ -44,7 +44,7 @@ def postOrderTraversal(rootNode):  # O(n), O(n)
     print(rootNode.data)
 
 import QueueLinkedList as queue 
-def levelOrderTraversal(rootNode):
+def levelOrderTraversal(rootNode):  # O(n), O(n)
     if not rootNode:
         return
     else:
@@ -61,7 +61,79 @@ def levelOrderTraversal(rootNode):
             
             if root.value.rightChild is not None:
                 customQ.enqueue(root.value.rightChild)
+
+def searchNodeBST(rootNode, nodeValue):     # O(logN), O(logN)
+    if nodeValue == rootNode.data:
+        print(nodeValue, "is FOUND!")
+    elif nodeValue < rootNode.data:
+        if rootNode.leftChild == None:
+            print(nodeValue, "is NOT FOUND!")
+        elif rootNode.leftChild.data == nodeValue:
+            print(nodeValue, "is FOUND!")
+            return
+        else:
+            searchNodeBST(rootNode.leftChild, nodeValue)
+    elif nodeValue > rootNode.data:
+        if rootNode.rightChild == None:
+            print(nodeValue, "is NOT FOUND!")
+        elif rootNode.rightChild.data == nodeValue:
+            print(nodeValue, "is FOUND!")
+            return
+        else:
+            searchNodeBST(rootNode.rightChild, nodeValue)
+
+
+# DELETION
+def findMinValueNode(rootNode):      # O(logN)
+    current = rootNode
+
+    while current:
+        current = current.leftChild
     
+    return current
+
+def deleteNode(rootNode, nodeValue):    # O(logN), O(logN)
+    if rootNode is None:
+        return rootNode
+    
+    # if nodeValue on LHS of BST
+    if nodeValue < rootNode.data:
+        rootNode.leftChild = deleteNode(rootNode.leftChild, nodeValue)
+    # if nodeValue on RHS of BST
+    elif nodeValue > rootNode.data:
+        rootNode.rightChild = deleteNode(rootNode.rightChild, nodeValue)
+    
+    else:
+        # leaf node value encounter
+        if rootNode.leftChild is None:
+            temp = rootNode.rightChild
+            rootNode = None
+            return temp
+        
+        # leaf node value encounter
+        if rootNode.rightChild is None:
+            temp = rootNode.leftChild
+            rootNode = None
+            return temp
+
+        # two child nodevalues encounter
+        temp = findMinValueNode(rootNode)
+        rootNode.data = temp.data
+        rootNode.rightChild = deleteNode(rootNode.rightChild, temp.data)
+    
+    return rootNode
+
+    
+def deleteBST(rootNode):        # O(1), O(1)
+    if not rootNode:
+        return None
+
+    rootNode.leftChild = None
+    rootNode.rightChild = None
+    rootNode.data = None
+
+    return "BST deletion successful!"
+
 newBST = BSTNode(None)      # O(1), O(1)
 insertNodeBST(newBST, 70)
 insertNodeBST(newBST, 50)
@@ -71,9 +143,9 @@ insertNodeBST(newBST, 45)
 insertNodeBST(newBST, 75)
 insertNodeBST(newBST, 90)
 
-print(newBST.data)
-print('newBST.leftChild.data', newBST.leftChild.data)
-print('newBST.rightChild.data', newBST.rightChild.data)
+#print(newBST.data)
+#print('newBST.leftChild.data', newBST.leftChild.data)
+#print('newBST.rightChild.data', newBST.rightChild.data)
 
 # DFS
 #print('preOrderTraversal\n')
@@ -84,4 +156,13 @@ print('newBST.rightChild.data', newBST.rightChild.data)
 #postOrderTraversal(newBST)
 
 # BFS
+#levelOrderTraversal(newBST)
+
+# SEARCH
+#searchNodeBST(newBST, 30)
+#searchNodeBST(newBST, 100)
+
+#deleteNode(newBST, 45)
+#deleteNode(newBST, 90)
+deleteNode(newBST, 50)
 levelOrderTraversal(newBST)
