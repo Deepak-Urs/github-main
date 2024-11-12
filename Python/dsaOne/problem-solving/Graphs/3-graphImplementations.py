a13f235e173f0273464a1dc10f4876c0d553c4dd
Deepak-Urs <gvdeepakurs2202@gmail.com>
2024-11-12 15:26:02 -0800

    adjacency list implementation

--- Python/dsaOne/problem-solving/Graphs/3-graphImplementations.py
+++ Python/dsaOne/problem-solving/Graphs/3-graphImplementations.py
@@ -19,3 +19,31 @@ def adjacencyMatrixGraph():
     print(adjMat)
 
 adjacencyMatrixGraph()
+
+def adjacencyListGraph():
+    # edgesList
+    # readList
+    # init map
+    # populate map
+
+    n = 5
+    edgesList = [
+        [1, 2], [2, 3], [3, 4], [4, 2], [1, 3]
+    ]
+    adjList = {}
+
+    for edge in edgesList:
+        a, b = edge[0], edge[1]
+
+        if a not in adjList:
+            adjList[a] = []
+        
+        if b not in adjList:
+            adjList[b] = []
+        
+        adjList[a].append(b)
+        adjList[b].append(a)
+    
+    print(adjList)
+
+adjacencyListGraph()
