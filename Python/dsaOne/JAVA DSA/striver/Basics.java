0dae1712decf5649316eda159285831dd6ec9bad
Deepak-Urs <gvdeepakurs2202@gmail.com>
2025-04-11 18:21:48 -0500

    striver: JAVA basics

---
+++ Python/dsaOne/JAVA DSA/striver/Basics.java
@@ -0,0 +1,12 @@
+class printTest {
+    public static void printText() {
+        System.out.println("Hello!");
+    }
+}
+
+public class Basics {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        printTest.printText();
+    }
+}
