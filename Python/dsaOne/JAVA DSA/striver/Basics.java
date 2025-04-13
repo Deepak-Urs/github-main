0271697cc3dcad45e8f7aa6b246e0b419bb02fd8
Deepak-Urs <gvdeepakurs2202@gmail.com>
2025-04-12 23:37:44 -0500

    striver: JAVA Objects

--- Python/dsaOne/JAVA DSA/striver/Basics.java
+++ Python/dsaOne/JAVA DSA/striver/Basics.java
@@ -1,12 +1,20 @@
-class printTest {
-    public static void printText() {
-        System.out.println("Hello!");
-    }
+class Test {
+        int age;
+
+        public void assignAge(int num) {
+            age = num;
+        }
 }
 
 public class Basics {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        printTest.printText();
+        Test t1 = new Test();
+        t1.assignAge(10);
+
+        Test t2 = new Test();
+        t2.assignAge(15);
+
+        System.out.println(t1.age);
+        System.out.println(t2.age);
     }
 }
